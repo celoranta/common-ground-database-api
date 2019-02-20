9f4d06d520bebad8370376f0c8e2c8536c7079a0
Christoper <celoranta@gmail.com>
2019-02-19 23:18:44 -0800

    Use backticks

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ var connection = mysql.createConnection({
  
 connection.connect();
  
-connection.query('CREATE TABLE IF NOT EXISTS tasks (' +
-  'task_id INT AUTO_INCREMENT,' +
-  'title VARCHAR(255) NOT NULL,' +
-  'start_date DATE,' +
-  'due_date DATE, ' +
-  'status TINYINT NOT NULL,'+
-  'priority TINYINT NOT NULL,'+
-  'description TEXT,'+
-  'PRIMARY KEY (task_id);'
+connection.query(
+`CREATE TABLE IF NOT EXISTS tasks (
+  task_id INT AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  start_date DATE,
+  due_date DATE, 
+  status TINYINT NOT NULL,
+  priority TINYINT NOT NULL,
+  description TEXT,
+  PRIMARY KEY (task_id);`
 )  , function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);

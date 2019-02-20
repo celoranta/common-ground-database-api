1a76196b4d0dcbc77c43eca1e5dccdad7ac5ca1b
Christoper <celoranta@gmail.com>
2019-02-19 23:21:45 -0800

    Different query

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ var connection = mysql.createConnection({
 connection.connect();
  
 connection.query(
-`CREATE TABLE IF NOT EXISTS tasks (
-  task_id INT AUTO_INCREMENT,
-  title VARCHAR(255) NOT NULL,
-  start_date DATE,
-  due_date DATE, 
-  status TINYINT NOT NULL,
-  priority TINYINT NOT NULL,
-  description TEXT,
-  PRIMARY KEY (task_id);`
+  `CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255) 
+);
+`
 )  , function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);

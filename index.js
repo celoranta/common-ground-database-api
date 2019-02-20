aee957acd41f4ddd5e88d5620cf809032a8cf142
Christoper <celoranta@gmail.com>
2019-02-19 22:58:59 -0800

    Query for database qty

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+connection.query('SELECT name FROM master.sys.databases', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });

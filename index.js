684797e492750955af0e4cf960318a937614e9b1
Christoper <celoranta@gmail.com>
2019-02-22 23:20:25 -0800

    Complete basic Identity tables and ERD

--- index.js
+++ index.js
@@ -97,12 +97,30 @@ PRIMARY KEY (ActID)
 };
 
 connection.query(
+`
+CREATE TABLE IF NOT EXISTS Clearances (
+ClearanceID Int NOT NULL AUTO_INCREMENT,
+ClearanceName varchar(255),
+CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (ClearanceID)
+);
+`
+)  , function (error, results, fields) {
+  if (error) throw error;
+};
+
+connection.query(
   `
-  CREATE TABLE IF NOT EXISTS Clearances (
-  ClearanceID Int NOT NULL AUTO_INCREMENT,
-  ClearanceName varchar(255),
+  CREATE TABLE IF NOT EXISTS Memberships (
+  Membership ID Int NOT NULL AUTO_INCREMENT,
+  PersonID Int NOT NULL,
+  ActID Int NOT NULL,
+  ClearanceID Int NOT NULL,
   CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (ClearanceID)
+  PRIMARY KEY (MembershipID),
+  FOREIGN KEY (PersonID) REFERENCES Persons(PersonID),
+  FOREIGN KEY (ActID) REFERENCES Acts(ActID),
+  FOREIGN KEY (ClearanceID) REFERENCES Clearances(ClearanceID)
   );
   `
   )  , function (error, results, fields) {

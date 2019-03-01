31fb87b2165cf2fcbd1bce1894c3cb3220d87b5e
Christoper <celoranta@gmail.com>
2019-03-01 14:00:01 -0800

    Ingetrate personNameType parameter into put

--- index.js
+++ index.js
@@ -236,31 +236,20 @@ app.get('/PersonNameTypes', function(req, res, err)  {
   );
 });
 
-app.put('/PersonNameTypes', function(req, res, err)  {   
+app.put('/PersonNameTypes/:PersonNameType', function(req, res, err)  {   
   PersonNameTypes
-  .findOrCreate({where: {NameType: 'surname'}})
+  .findOrCreate({where: {NameType: req.params.personNameType}})
   .spread((personNameType, created) => {
     console.log(personNameType.get({
       plain: true
     }))
     console.log(personNameType)
 
-    /*
-     findOrCreate returns an array containing the object that was found or created and a boolean that will be true if a new object was created and false if not, like so:
-
-    [ {
-        username: 'sdepold',
-        job: 'Technical Lead JavaScript',
-        id: 1,
-        createdAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET),
-        updatedAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET)
-      },
-      true ]
-
+/*
  In the example above, the "spread" on line 39 divides the array into its 2 parts and passes them as arguments to the callback function defined beginning at line 39, which treats them as "user" and "created" in this case. (So "user" will be the object from index 0 of the returned array and "created" will equal "true".)
     */
   })
-  return res.send('Added new object');
+  return res.send('Added new object: ' + personNameType.NameType);
 });
 
 app.post('/Persons', (req, res) => {   

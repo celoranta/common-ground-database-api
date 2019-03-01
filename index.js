f15fbdcc3bb4e6081e6b1c49586c6d2c1d12d58f
Christoper <celoranta@gmail.com>
2019-03-01 08:00:39 -0800

    Implement Sequelize

--- index.js
+++ index.js
@@ -4,141 +4,193 @@ var mysql = require('mysql');
 var env = require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const Sequelize = require('sequelize');
 
 var app = express();
 const port = process.env.PORT;
  /* set the bodyParser to parse the urlencoded post data */
  app.use(bodyParser.urlencoded({ extended: true }))
 
-//Test Database
- var connection = getConnectedDb();
+ //Create Database
+var connection = mysql.createConnection({
+  host     : process.env.HOST,
+  user     : process.env.DB_USER,
+  password : process.env.MYSQL_PASSWORD,
+  database : process.env.MYSQL_DATABASE
+});
+connection.connect();
 
-connection.query(
+ connection.query(
   `
   CREATE DATABASE IF NOT EXISTS commonground;
   `
   )  , function (error, results, fields) {
     if (error) throw error;
   };
-
-connection.query(
-  `
-  CREATE TABLE IF NOT EXISTS PersonNameTypes (
-  PersonNameTypeID int NOT NULL AUTO_INCREMENT,
-  NameType varchar(255) NOT NULL,
-  CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (PersonNameTypeID)
-  );
-  `
-  )  , function (error, results, fields) {
-    if (error) throw error;
-  };
-
-connection.query(
-  `
-  CREATE TABLE IF NOT EXISTS Persons (
-  PersonID int NOT NULL AUTO_INCREMENT,
-  CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (PersonID)
-  );
-  `
-  )  , function (error, results, fields) {
-    if (error) throw error;
-  };
-
-connection.query(
-`
-CREATE TABLE IF NOT EXISTS PersonNames (
-PersonNameID int NOT NULL AUTO_INCREMENT,
-CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PersonNameType Int NOT NULL,
-PersonNameString varchar(255) NOT NULL,
-PersonID Int NOT NULL,
-PRIMARY KEY (PersonNameID),
-FOREIGN KEY (PersonNameType) REFERENCES PersonNameTypes(PersonNameTypeID),
-FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
-);
-`
-)  , function (error, results, fields) {
-  if (error) throw error;
-};
-
-connection.query(
-`
-CREATE TABLE IF NOT EXISTS EmailAddresses (
-EmailAddressID Int NOT NULL AUTO_INCREMENT,
-EmailAddress varchar(255),
-PersonID Int NOT NULL,
-CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (EmailAddressID),
-FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
-);
-`
-)  , function (error, results, fields) {
-  if (error) throw error;
-};
-
-connection.query(
-`
-CREATE TABLE IF NOT EXISTS Acts (
-ActID Int NOT NULL AUTO_INCREMENT,
-ActName varchar(255),
-CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (ActID)
-);
-`
-)  , function (error, results, fields) {
-  if (error) throw error;
-};
-
-connection.query(
-`
-CREATE TABLE IF NOT EXISTS Clearances (
-ClearanceID Int NOT NULL AUTO_INCREMENT,
-ClearanceName varchar(255),
-CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (ClearanceID)
-);
-`
-)  , function (error, results, fields) {
-  if (error) throw error;
-};
-
-connection.query(
-`
-CREATE TABLE IF NOT EXISTS Memberships (
-MembershipID Int NOT NULL AUTO_INCREMENT,
-PersonID Int NOT NULL,
-ActID Int NOT NULL,
-ClearanceID Int NOT NULL,
-CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (MembershipID),
-FOREIGN KEY (PersonID) REFERENCES Persons(PersonID),
-FOREIGN KEY (ActID) REFERENCES Acts(ActID),
-FOREIGN KEY (ClearanceID) REFERENCES Clearances(ClearanceID)
-);
-`
-)  , function (error, results, fields) {
-  if (error) throw error;
-};
-
-connection.query(
-`
-CREATE TABLE IF NOT EXISTS BandContactInfo (
-ID int NOT NULL AUTO_INCREMENT,
-CityName varchar(255),
-StateOrProvince varChar(255),
-PhoneNumber varChar(255),
-EmailAddress varChar(255),
-PRIMARY KEY (ID)
-);
-`
-)  , function (error, results, fields) {
-  if (error) throw error;
-};
- 
 connection.end();
+
+
+
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.DB_USER, process.env.MYSQL_PASSWORD , {
+  host: process.env.HOST,
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+  const PersonNameType = sequelize.define('PersonNameType', {
+    NameType: {
+      type: Sequelize.STRING
+    },
+    CreatedAt: {
+      type: Sequelize.DATE
+    },
+
+  });
+  
+  // force: true will drop the table if it already exists
+  PersonNameType.sync({force: true}).then(() => {
+    // Table created
+    // return User.create({
+    //   firstName: 'John',
+    //   lastName: 'Hancock'
+    // });
+  });
+
+
+
+// connection.query(
+//   `
+//   CREATE TABLE IF NOT EXISTS PersonNameTypes (
+//   PersonNameTypeID int NOT NULL AUTO_INCREMENT,
+//   NameType varchar(255) NOT NULL,
+//   CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//   PRIMARY KEY (PersonNameTypeID)
+//   );
+//   `
+//   )  , function (error, results, fields) {
+//     if (error) throw error;
+//   };
+
+// connection.query(
+//   `
+//   CREATE TABLE IF NOT EXISTS Persons (
+//   PersonID int NOT NULL AUTO_INCREMENT,
+//   CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//   PRIMARY KEY (PersonID)
+//   );
+//   `
+//   )  , function (error, results, fields) {
+//     if (error) throw error;
+//   };
+
+// connection.query(
+// `
+// CREATE TABLE IF NOT EXISTS PersonNames (
+// PersonNameID int NOT NULL AUTO_INCREMENT,
+// CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+// PersonNameType Int NOT NULL,
+// PersonNameString varchar(255) NOT NULL,
+// PersonID Int NOT NULL,
+// PRIMARY KEY (PersonNameID),
+// FOREIGN KEY (PersonNameType) REFERENCES PersonNameTypes(PersonNameTypeID),
+// FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+// );
+// `
+// )  , function (error, results, fields) {
+//   if (error) throw error;
+// };
+
+// connection.query(
+// `
+// CREATE TABLE IF NOT EXISTS EmailAddresses (
+// EmailAddressID Int NOT NULL AUTO_INCREMENT,
+// EmailAddress varchar(255),
+// PersonID Int NOT NULL,
+// CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+// PRIMARY KEY (EmailAddressID),
+// FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+// );
+// `
+// )  , function (error, results, fields) {
+//   if (error) throw error;
+// };
+
+// connection.query(
+// `
+// CREATE TABLE IF NOT EXISTS Acts (
+// ActID Int NOT NULL AUTO_INCREMENT,
+// ActName varchar(255),
+// CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+// PRIMARY KEY (ActID)
+// );
+// `
+// )  , function (error, results, fields) {
+//   if (error) throw error;
+// };
+
+// connection.query(
+// `
+// CREATE TABLE IF NOT EXISTS Clearances (
+// ClearanceID Int NOT NULL AUTO_INCREMENT,
+// ClearanceName varchar(255),
+// CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+// PRIMARY KEY (ClearanceID)
+// );
+// `
+// )  , function (error, results, fields) {
+//   if (error) throw error;
+// };
+
+// connection.query(
+// `
+// CREATE TABLE IF NOT EXISTS Memberships (
+// MembershipID Int NOT NULL AUTO_INCREMENT,
+// PersonID Int NOT NULL,
+// ActID Int NOT NULL,
+// ClearanceID Int NOT NULL,
+// CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+// PRIMARY KEY (MembershipID),
+// FOREIGN KEY (PersonID) REFERENCES Persons(PersonID),
+// FOREIGN KEY (ActID) REFERENCES Acts(ActID),
+// FOREIGN KEY (ClearanceID) REFERENCES Clearances(ClearanceID)
+// );
+// `
+// )  , function (error, results, fields) {
+//   if (error) throw error;
+// };
+
+// connection.query(
+// `
+// CREATE TABLE IF NOT EXISTS BandContactInfo (
+// ID int NOT NULL AUTO_INCREMENT,
+// CityName varchar(255),
+// StateOrProvince varChar(255),
+// PhoneNumber varChar(255),
+// EmailAddress varChar(255),
+// PRIMARY KEY (ID)
+// );
+// `
+// )  , function (error, results, fields) {
+//   if (error) throw error;
+// };
+ 
+// connection.end();
 
 //MARK: Define API Endpoints
 
@@ -162,13 +214,13 @@ app.delete('/', (req, res) => {
   return res.send('Received a DELETE message');
 });
 
-app.get('/Persons', function(req, res, err)  {   
-  connection.query('SELECT * FROM Persons AS solution', function (err, rows, fields) {
-    if (err) throw err
+// app.get('/Persons', function(req, res, err)  {   
+//   connection.query('SELECT * FROM Persons AS solution', function (err, rows, fields) {
+//     if (err) throw err
   
-    console.log('The solution is: ', rows[0].solution)
-  })
-});
+//     console.log('The solution is: ', rows[0].solution)
+//   })
+// });
 
 app.post('/Persons', (req, res) => {   
   return res.send('POST method for Persons object'
@@ -192,31 +244,22 @@ app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
 
-function getConnectedDb(){
-var connection = mysql.createConnection({
-  host     : process.env.HOST,
-  user     : process.env.DB_USER,
-  password : process.env.MYSQL_PASSWORD,
-  database : process.env.MYSQL_DATABASE
-});
-connection.connect();
-return connection;
-}
+
 
 //MARK: Helper Functions
 
-async function allPersons() {
-  var connection = getConnectedDb();
-  var uresults;
-  await connection.query("SELECT * FROM Persons;")
-   , function (error, results, fields) {
-    if (error) throw error;
-    uresults = results
-  };
-    connection.end();
-    console.log(uresults);
-  return uresults;
-}
+// async function allPersons() {
+//   var connection = getConnectedDb();
+//   var uresults;
+//   await connection.query("SELECT * FROM Persons;")
+//    , function (error, results, fields) {
+//     if (error) throw error;
+//     uresults = results
+//   };
+//     connection.end();
+//     console.log(uresults);
+//   return uresults;
+// }
 
 
 

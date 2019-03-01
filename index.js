fc462714e7b5d812ac431257569d1b90b7a58d3e
Christoper <celoranta@gmail.com>
2019-03-01 11:54:25 -0800

    mysql2

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ sequelize
   });
   
   // force: true will drop the table if it already exists
-  PersonNameTypes.sync({force: true}).then(() => {
+  PersonNameTypes.sync({force: false}).then(() => {
     // Table created
     // return User.create({
     //   firstName: 'John',
@@ -214,10 +214,36 @@ app.get('/PersonNameTypes', function(req, res, err)  {
   PersonNameTypes.findAll()
   .then(personNameTypes => {
     console.log(personNameTypes);
+    return personNameTypes
   }
-
   );
 });
+
+app.put('/PersonNameTypes', function(req, res, err)  {   
+  PersonNameTypes
+  .findOrCreate({where: {username: process.env.DB_USER}, defaults: {nameType: 'surname'}})
+  .spread((user, created) => {
+    console.log(user.get({
+      plain: true
+    }))
+    console.log(created)
+
+    /*
+     findOrCreate returns an array containing the object that was found or created and a boolean that will be true if a new object was created and false if not, like so:
+
+    [ {
+        username: 'sdepold',
+        job: 'Technical Lead JavaScript',
+        id: 1,
+        createdAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET),
+        updatedAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET)
+      },
+      true ]
+
+ In the example above, the "spread" on line 39 divides the array into its 2 parts and passes them as arguments to the callback function defined beginning at line 39, which treats them as "user" and "created" in this case. (So "user" will be the object from index 0 of the returned array and "created" will equal "true".)
+    */
+  })
+};
 
 app.post('/Persons', (req, res) => {   
   return res.send('POST method for Persons object'
